d088614ddc52182338e689c705798771bc0800f0
adedewa <ad19wa@gmail.com>
2022-12-21 22:11:00 +0700

    komentar

---
+++ android/app/src/main/kotlin/com/example/uas_adedewa/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.uas_adedewa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/request/berita_request.dart
@@ -0,0 +1,10 @@
+
+import '../model/berita_model.dart';
+
+class BeritaRequest {
+  Future<BeritaModel> getBerita() async {
+    Response response = await Dio()
+        .get('https://api-berita-indonesia.vercel.app/antara/terbaru');
+    return BeritaModel.fromJson(response.data);
+  }
+}
